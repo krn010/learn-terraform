ad3d81e8c66c4302bf8d1ddfdd2bf9bb69298551
metal <metalheadkrn@gmail.com>
2024-09-06 21:19:08 +0530

    Variables and output in terraform

--- 02-vars/output.tf
+++ 02-vars/output.tf
@@ -1,3 +1,16 @@
 output "v1" {
   value = var.v1
 }
+
+output "First value in list" {
+  value = var.v2[0]
+}
+
+output "Second value in list" {
+  value = var.v2[1]
+}
+
+output " Value in map " {
+  value = var.v3["abc"]
+}
+
