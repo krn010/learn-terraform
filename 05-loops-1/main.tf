a756c41b81780e531630d7eb908ffa8d53d7f01d
metal <metalheadkrn@gmail.com>
2024-09-07 16:05:52 +0530

    Creating dummy instances in one go

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -7,3 +7,14 @@ resource "null_resource" "test" {
 output "test" {
   value = null_resource.test.*.id
 }
+
+resource "aws_instance" "test" {
+  count = 3
+  ami                    = "ami-0b4f379183e5706b9"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01feec8e482efd795"]
+
+  tags = {
+    Name = "test"
+  }
+}
