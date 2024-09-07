c289f243bb7b93dd4941f1f09acd1c16ba8c7849
metal <metalheadkrn@gmail.com>
2024-09-07 14:07:38 +0530

    Creating EC2 Instance and records via terraform

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z08439573VJI40CLVIM4E"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "backend" {
   ami                    = "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
@@ -16,6 +24,15 @@ resource "aws_instance" "backend" {
   }
 }
 
+resource "aws_route53_record" "backend" {
+  zone_id = "Z08439573VJI40CLVIM4E"
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+
 resource "aws_instance" "mysql" {
   ami                    = "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
@@ -23,4 +40,12 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql-dev"
   }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z08439573VJI40CLVIM4E"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
