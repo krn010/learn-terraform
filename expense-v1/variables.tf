a19c05de039ad9c51200d893b0f8d7718fc7fc2e
metal <metalheadkrn@gmail.com>
2024-09-07 14:27:26 +0530

    Creating EC2 Instance and records via terraform and using variables

---
+++ expense-v1/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-01feec8e482efd795"]
+}
+
+variable "zone_id" {
+  default = "Z08439573VJI40CLVIM4E"
+}
+
