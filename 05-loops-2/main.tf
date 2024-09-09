aa4ef52f7ab1848f3536ed77dab5a4084bfec562
metal <metalheadkrn@gmail.com>
2024-09-09 20:48:28 +0530

    For loops example in terraform

---
+++ 05-loops-2/main.tf
@@ -0,0 +1,27 @@
+variable "fruits1" {
+  default = {
+    apple  = 10
+    banana = 100
+  }
+}
+
+variable "fruits2" {
+  default = {
+    apple = {
+      name = "apple"
+      quantity = 10
+    }
+    banana = {
+      name = "banana"
+      quantity = 100
+    }
+  }
+}
+
+
+resource "null_resource" "test1" {
+  for_each = var.fruits1
+  provisioner "local-exec" {
+    commmand = "echo ${each.key} = ${each.value}"
+  }
+}
